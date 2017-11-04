2f572467536090238cf0a3a5f2640b33a51dc071
Tõnis Kasekamp <toniskasekamp@gmail.com>
2017-11-04 18:53:58 +0200

    Log resources by date
    New methods about 2x slower than old.

--- logs/log_service.py
+++ logs/log_service.py
@@ -1,14 +1,14 @@
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 from opyenxes.classification.XEventAttributeClassifier import XEventAttributeClassifier
 
 
 def events_by_date(logs):
-    """Creates dict of events by timestamp
+    """Creates dict of events by date ordered by date
 
     Expected result
     {'2010-12-30': 7, '2011-01-06': 8}
     """
-    classifier = XEventAttributeClassifier("Resource", ["time:timestamp"])
+    classifier = XEventAttributeClassifier("Event", ["time:timestamp"])
     stamp_dict = defaultdict(lambda: 0)
     for log in logs:
         for trace in log:
@@ -16,4 +16,27 @@ def events_by_date(logs):
                 timestamp = classifier.get_class_identity(event)
                 date = timestamp.split("T")[0]
                 stamp_dict[date] += 1
-    return stamp_dict
+    return OrderedDict(sorted(stamp_dict.items()))
+
+def resources_by_date(logs):
+    """Creates dict of used unique resources orederd by date
+
+    Resource and timestamp delimited by &&. If this is in resources name, bad stuff will happen.
+    Returns a dict with a date and the number of unique resources used on that day.
+    {'2010-12-30': 7, '2011-01-06': 8}
+    """
+    classifier = XEventAttributeClassifier("Resource", ["Resource", "time:timestamp"])
+    stamp_dict = defaultdict(lambda: [])
+    for log in logs:
+        for trace in log:
+            for event in trace:
+                resource_and_timestamp = classifier.get_class_identity(event)
+                resource = resource_and_timestamp.split("&&")[0]
+                timestamp = resource_and_timestamp.split("&&")[1]
+                date = timestamp.split("T")[0]
+                stamp_dict[date].append(resource)
+
+    for key, value in stamp_dict.items():
+        stamp_dict[key] = len(set(value))
+
+    return OrderedDict(sorted(stamp_dict.items()))
--- logs/tests.py
+++ logs/tests.py
@@ -1,7 +1,6 @@
 from django.test import SimpleTestCase
 from opyenxes.data_in.XesXmlParser import XesXmlParser
-from .log_service import events_by_date
-
+from .log_service import events_by_date, resources_by_date
 
 class LogTest(SimpleTestCase):
 
@@ -11,3 +10,12 @@ class LogTest(SimpleTestCase):
         result = events_by_date(logs)
         self.assertEqual(18, len(result.keys()))
         self.assertEqual(4, result['2011-01-08'])
+
+    def test_resources_by_date(self):
+        with open("log_cache/general_example.xes") as file:
+            logs = XesXmlParser().parse(file)
+        result = resources_by_date(logs)
+        self.assertEqual(18, len(result.keys()))
+        self.assertEqual(4, result['2010-12-30'])
+        self.assertEqual(3, result['2011-01-08'])
+        self.assertEqual(1, result['2011-01-20'])

8cae08e59ac34208314c98a64d126318144e333e
Tõnis Kasekamp <tonis.kasekamp@salemove.com>
2017-11-06 20:38:32 +0200

    Fix boolean encoder

--- encoders/boolean_frequency.py
+++ encoders/boolean_frequency.py
@@ -18,23 +18,23 @@ def frequency(data):
 
 def encode_boolean_frequency(log: list, encoding='boolean'):
     """Internal method for both boolean and frequency. Only dif is __append_item"""
-    events = unique_events(log)
+    event_names = unique_events(log)
 
-    columns = np.append(events, list(DEFAULT_COLUMNS))
+    columns = np.append(event_names, list(DEFAULT_COLUMNS))
     encoded_data = []
 
     for trace in log:
         trace_name = classifier.get_class_identity(trace)
-        event_happened = [False] * len(events)
-        for event in trace:
+        # starts with all False, changes to event
+        event_happened = [False] * len(event_names)
+        for event_index, event in enumerate(trace):
             case_data = []
-            event_name = classifier.get_class_identity(event)
-            event_index = events.index(event_name)
-            event_happened[event_index] = True
+            update_event_happened(event, event_names, event_happened)
             case_data += event_happened
 
             case_data.append(trace_name)
-            case_data.append(event_index)
+            # Start counting at 1
+            case_data.append(event_index + 1)
             remaining_time = remaining_time_event(trace, event)
             case_data.append(remaining_time)
             elapsed_time = elapsed_time_event(trace, event)
@@ -42,3 +42,10 @@ def encode_boolean_frequency(log: list, encoding='boolean'):
             encoded_data.append(case_data)
 
     return pd.DataFrame(columns=columns, data=encoded_data)
+
+
+def update_event_happened(event, event_names: list, event_happened: list):
+    """Updates the event_happened list at event index"""
+    event_name = classifier.get_class_identity(event)
+    event_index = event_names.index(event_name)
+    event_happened[event_index] = True
--- encoders/tests/test_boolean.py
+++ encoders/tests/test_boolean.py
@@ -7,10 +7,10 @@ from logs.file_service import get_logs
 class TestBooleanGeneral(TestCase):
     def setUp(self):
         self.log = get_logs("log_cache/general_example.xes")[0]
+        self.df = boolean(self.log)
 
     def test_shape(self):
-        df = boolean(self.log)
-
+        df = self.df
         names = ['register request', 'examine casually', 'check ticket', 'decide',
                  'reinitiate request', 'examine thoroughly', 'pay compensation',
                  'reject request', 'case_id', 'event_nr', 'remaining_time',
@@ -20,7 +20,7 @@ class TestBooleanGeneral(TestCase):
         self.assertEqual((42, 12), df.shape)
 
     def test_row(self):
-        df = boolean(self.log)
+        df = self.df
 
         row = df[(df.event_nr == 2) & (df.case_id == '2')].iloc[0]
 
@@ -30,3 +30,18 @@ class TestBooleanGeneral(TestCase):
         self.assertFalse(row['decide'])
         self.assertEqual(2400.0, row.elapsed_time)
         self.assertEqual(777180.0, row.remaining_time)
+
+    def test_row2(self):
+        df = self.df
+        row = df[(df.event_nr == 5) & (df.case_id == '2')].iloc[0]
+
+        self.assertTrue(row['register request'])
+        self.assertTrue(row['examine casually'])
+        self.assertTrue(row['check ticket'])
+        self.assertTrue(row['decide'])
+        self.assertFalse(row['reinitiate request'])
+        self.assertFalse(row['examine thoroughly'])
+        self.assertTrue(row['pay compensation'])
+        self.assertFalse(row['reject request'])
+        self.assertEqual(779580.0, row.elapsed_time)
+        self.assertEqual(0.0, row.remaining_time)

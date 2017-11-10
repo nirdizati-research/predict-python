f8f04addc4d3ad6afdb12bd2c04dda6f843a6b3e
Tõnis Kasekamp <toniskasekamp@gmail.com>
2017-11-10 21:22:41 +0200

    Regression done

--- core/regression.py
+++ core/regression.py
@@ -1,3 +1,4 @@
+import random
 from math import sqrt
 
 import numpy as np
@@ -8,7 +9,10 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import Lasso
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error
-from core.common import encode, calculate_results
+
+from core.common import encode
+
+pd.options.mode.chained_assignment = None
 
 
 def regression(job):
@@ -18,15 +22,15 @@ def regression(job):
     train_data, test_data, original_test_data = prep_data(df)
 
     if job.clustering == "kmeans":
-        results_df = kmeans_clustering(original_test_data, train_data, regressor, job)
+        results_df = kmeans_clustering(original_test_data, train_data, regressor)
     else:
-        results_df = no_clustering(original_test_data, train_data, test_data, regressor, job)
+        results_df = no_clustering(original_test_data, train_data, test_data, regressor)
 
-    results = calculate_results(results_df, job)
+    results = prepare_results(results_df, job)
     return results
 
 
-def kmeans_clustering(original_test_data, train_data, regressor, job):
+def kmeans_clustering(original_test_data, train_data, regressor):
     estimator = KMeans(n_clusters=3)
     estimator.fit(train_data)
 
@@ -34,56 +38,53 @@ def kmeans_clustering(original_test_data, train_data, regressor, job):
         i: original_test_data.iloc[np.where(estimator.predict(original_test_data.drop('case_id', 1)) == i)[0]]
         for i in range(estimator.n_clusters)}
     cluster_lists = {i: train_data.iloc[np.where(estimator.labels_ == i)[0]] for i in range(estimator.n_clusters)}
-    # print original_cluster_lists
-    write_header = True
+    result_data = None
     for cluster_list in cluster_lists:
-        clustered_train_data = cluster_lists[cluster_list]
-
-        clustered_test_data = original_cluster_lists[cluster_list]
         original_test_clustered_data = original_cluster_lists[cluster_list]
-        clustered_test_data = clustered_test_data.drop('case_id', 1)
-        clustered_test_data = clustered_test_data.drop('remaining_time', 1)
+        if original_test_clustered_data.shape[0] == 0:
+            pass
+        else:
+            clustered_train_data = cluster_lists[cluster_list]
+            clustered_test_data = original_cluster_lists[cluster_list]
+            clustered_test_data = clustered_test_data.drop('case_id', 1)
+            clustered_test_data = clustered_test_data.drop('remaining_time', 1)
 
-        y = clustered_train_data['remaining_time']
-        clustered_train_data = clustered_train_data.drop('remaining_time', 1)
+            y = clustered_train_data['remaining_time']
+            clustered_train_data = clustered_train_data.drop('remaining_time', 1)
 
-        regressor.fit(clustered_train_data, y)
+            regressor.fit(clustered_train_data, y)
+            original_test_clustered_data['prediction'] = regressor.predict(clustered_test_data)
+            if result_data is None:
+                result_data = original_test_clustered_data
+            else:
+                result_data = result_data.append(original_test_clustered_data)
+    return result_data
 
-        original_test_clustered_data['prediction'] = regressor.predict(clustered_test_data)
 
-        path = job.get_results_path()
-        write_header = write_to_path(original_test_clustered_data, path, write_header)
-
-
-def no_clustering(original_test_data, train_data, test_data, regressor, job):
+def no_clustering(original_test_data, train_data, test_data, regressor):
     y = train_data['remaining_time']
-
     train_data = train_data.drop('remaining_time', 1)
-
     regressor.fit(train_data, y)
-
     original_test_data['prediction'] = regressor.predict(test_data)
-    original_test_data.to_csv(job.get_results_path(), sep=',', mode='w+', index=False)
+    return original_test_data
 
 
-def calculate_results(job):
-    df = pd.read_csv(filepath_or_buffer=job.get_results_path(), header=0, index_col=0)
+def prepare_results(df, job):
+    # TODO are remaining time in seconds or hours?
     df['remaining_time'] = df['remaining_time'] / 3600
     df['prediction'] = df['prediction'] / 3600
     rmse = sqrt(mean_squared_error(df['remaining_time'], df['prediction']))
     mae = mean_absolute_error(df['remaining_time'], df['prediction'])
     rscore = metrics.r2_score(df['remaining_time'], df['prediction'])
 
-    field_names = ['Run', 'Rmse', 'Mae', 'Rscore']
-    row = {'Run': job.method_val(), 'Rmse': rmse, 'Mae': mae, 'Rscore': rscore}
-    write_results_to_general(job, field_names, row)
-    # DB code. if implemented, remove the above stuff
-    row2 = {'uuid': job.uuid, 'rmse': rmse, 'mae': mae, 'rscore': rscore}
-    write_results_to_db(row2)
+    row = {'run': job.method_val(), 'rmse': rmse, 'mae': mae, 'rscore': rscore}
+    print("calculation done")
+    print(row)
+    return row
+
 
 def split_data(data):
     cases = data['case_id'].unique()
-    import random
     random.shuffle(cases)
 
     cases_train_point = int(len(cases) * 0.8)
---
+++ core/tests/test_regression.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+
+from core.job import Job
+from core.regression import regression
+
+
+class TestRegression(TestCase):
+    """Proof of concept tests"""
+
+    def get_job(self):
+        json = dict()
+        json['uuid'] = "ads69"
+        json["clustering"] = "kmeans"
+        json["status"] = "completed"
+        json["log"] = "Production.xes"
+        json["regression"] = "linear"
+        json["encoding"] = "simpleIndex"
+        json["timestamp"] = "Oct 03 2017 13:26:53"
+        json["rule"] = "remaining_time"
+        json["type"] = "Regression"
+        return Job(json)
+
+    def test_reg_linear(self):
+        job = self.get_job()
+        job.clustering = 'None'
+        regression(job)
+
+    def test_reg_randomforest(self):
+        job = self.get_job()
+        job.regression = 'randomforest'
+        regression(job)
+
+    def test_reg_lasso(self):
+        job = self.get_job()
+        job.regression = 'lasso'
+        regression(job)

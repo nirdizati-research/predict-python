1eb4cdc597b8c0a824ffb6fe70ca288833344413
Tõnis Kasekamp <tonis.kasekamp@salemove.com>
2017-11-14 23:28:35 +0200

    Add error field to job model

--- jobs/models.py
+++ jobs/models.py
@@ -1,7 +1,25 @@
 from django.db import models
 
+from core.constants import REGRESSION, CLASSIFICATION, NEXT_ACTIVITY
 from logs.models import Log
 from jsonfield import JSONField
+
+CREATED = 'created'
+COMPLETED = 'completed'
+ERROR = 'error'
+RUNNING = 'running'
+
+TYPES = (
+    (CLASSIFICATION, 'Classification'),
+    (REGRESSION, 'Regression'),
+    (NEXT_ACTIVITY, 'Next activity'),
+)
+STATUSES = (
+    (CREATED, 'Created'),
+    (COMPLETED, 'Completed'),
+    (ERROR, 'Error'),
+    (RUNNING, 'Running'),
+)
 
 
 class BaseModel(models.Model):
@@ -14,6 +32,7 @@ class BaseModel(models.Model):
 
 class Job(BaseModel):
     config = JSONField()
-    status = models.CharField(default='created', max_length=200)
+    error = models.CharField(default='', max_length=200)
+    status = models.CharField(choices=STATUSES, default=CREATED, max_length=20)
     result = JSONField(default={})
-    type = models.CharField(max_length=20)
+    type = models.CharField(choices=TYPES, max_length=20)

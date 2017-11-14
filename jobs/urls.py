b652ae479308e598d4137ef4c089fe52a62abe61
Tõnis Kasekamp <tonis.kasekamp@salemove.com>
2017-11-14 23:10:35 +0200

    Add job detail view

--- jobs/urls.py
+++ jobs/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.job_list, name='jobs'),
+    url(r'^(?P<pk>[0-9]+)$', views.job_detail),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,4 +1,5 @@
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -23,3 +24,18 @@ def job_list(request):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+
+
+@api_view(['GET'])
+def job_detail(request, pk):
+    """
+    Retrieve job by id.
+    """
+    try:
+        snippet = Job.objects.get(pk=pk)
+    except Job.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = JobSerializer(snippet)
+        return Response(serializer.data)

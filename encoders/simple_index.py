6e6400c5950878a5da270c2134fb411c7f09f762
Tõnis Kasekamp <tonis.kasekamp@salemove.com>
2017-11-24 00:16:59 +0200

    SimpleIndex fixes
    Same logic as NextActivity

--- encoders/simple_index.py
+++ encoders/simple_index.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from opyenxes.classification.XEventAttributeClassifier import XEventAttributeClassifier
 
-from .log_util import remaining_time_id, elapsed_time_id, DEFAULT_COLUMNS
+from .log_util import remaining_time_id, elapsed_time_id
 
 CLASSIFIER = XEventAttributeClassifier("Trace name", ["concept:name"])
 
@@ -12,9 +12,7 @@ def simple_index(log: list, event_names: list, prefix_length=1, next_activity=Fa
     return encode_simple_index(log, event_names, prefix_length)
 
 
-def encode_simple_index(log: list, event_names: list, prefix_length: int, ):
-    # Events up to prefix_length
-    events_to_consider = event_names[:prefix_length]
+def encode_simple_index(log: list, event_names: list, prefix_length: int):
     columns = __create_columns(prefix_length)
     encoded_data = []
 
@@ -24,11 +22,14 @@ def encode_simple_index(log: list, event_names: list, prefix_length: int, ):
         trace_row = []
         trace_name = CLASSIFIER.get_class_identity(trace)
         trace_row.append(trace_name)
-        trace_row.append(prefix_length)
         trace_row.append(remaining_time_id(trace, prefix_length))
         trace_row.append(elapsed_time_id(trace, prefix_length))
-        for idx, _ in enumerate(events_to_consider):
-            trace_row.append(idx + 1)
+        for idx, event in enumerate(trace):
+            if idx == prefix_length:
+                break
+            event_name = CLASSIFIER.get_class_identity(event)
+            event_id = event_names.index(event_name)
+            trace_row.append(event_id + 1)
         encoded_data.append(trace_row)
 
     return pd.DataFrame(columns=columns, data=encoded_data)
@@ -62,7 +63,7 @@ def encode_next_activity(log: list, event_names: list, prefix_length: int):
 
 
 def __create_columns(prefix_length: int):
-    columns = list(DEFAULT_COLUMNS)
+    columns = ["trace_id", "remaining_time", "elapsed_time"]
     for i in range(1, prefix_length + 1):
         columns.append("prefix_" + str(i))
     return columns

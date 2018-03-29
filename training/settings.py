a41984a8bc54ef8aceb502b96affb3181d331bb4
Tõnis Kasekamp <toniskasekamp@gmail.com>
2018-03-29 14:44:30 +0300

    Allow all hosts
    For deployment to UT servers.

--- training/settings.py
+++ training/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'mt-0m%5!@ef9lcoga)nu@c9=ai@2_9l!*6v@u(^*zi-9w=882-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 

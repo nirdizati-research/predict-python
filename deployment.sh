24906c0a101a4e6545477821992d3b2e49935993
Tõnis Kasekamp <tonis.kasekamp@salemove.com>
2018-03-30 14:45:05 +0300

    Deploy another rqworker

--- deployment.sh
+++ deployment.sh
@@ -3,3 +3,4 @@ sudo pkill -f python
 # Server has both python and python3
 sudo nohup python3 manage.py runserver 0.0.0.0:80 &
 sudo nohup python3 manage.py rqworker default &
+sudo nohup python3 manage.py rqworker default &

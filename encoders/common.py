ff6d95675383bb28f783d9984e91035c146327e9
Tõnis Kasekamp <tonis.kasekamp@salemove.com>
2018-04-24 00:13:45 +0300

    Cleanup

--- encoders/common.py
+++ encoders/common.py
@@ -9,7 +9,7 @@ from .simple_index import simple_index
 
 def encode_label_logs(training_log: list, test_log: list, encoding_type: str, job_type: str, label: LabelContainer,
                       prefix_length=1, zero_padding=False):
-    """Encodes and labelstest set and training set as data frames
+    """Encodes and labels test set and training set as data frames
 
     :param prefix_length only for SIMPLE_INDEX, COMPLEX, LAST_PAYLOAD
     :returns training_df, test_df
@@ -36,19 +36,6 @@ def encode_label_log(run_log: list, encoding_type: str, job_type: str, label: La
         if label.type == REMAINING_TIME or label.type == ATTRIBUTE_NUMBER:
             return label_boolean(encoded_log, label)
     return encoded_log
-
-
-# def encode_logs(training_log: list, test_log: list, label: LabelContainer, encoding_type: str, job_type: str,
-#                 prefix_length=1, zero_padding=False):
-#     """Encodes test set and training set as data frames
-#
-#     :param prefix_length only for SIMPLE_INDEX, COMPLEX, LAST_PAYLOAD
-#     :returns training_df, test_df
-#     """
-#     event_names = unique_events2(training_log, test_log)
-#     training_df = encode_log(training_log, encoding_type, label, prefix_length, event_names, zero_padding=zero_padding)
-#     test_df = encode_log(test_log, encoding_type, label, prefix_length, event_names, zero_padding=zero_padding)
-#     return training_df, test_df
 
 
 def encode_log(run_log: list, encoding_type: str, label: LabelContainer, prefix_length=1, event_names=None,
--- encoders/label_container.py
+++ encoders/label_container.py
@@ -1,24 +1,3 @@
-"""Add a label to an encoded log
-
-Steps
-1. Encoded log
-2. Define labelling types
-    remaining_time
-    next_activity
-    attribute_number
-    attribute_string
-
-Encoding
-Post processing if needed
-
-
-def labelling
-    enc = encodedLog
-    if reg
-        rename column
-    elif class
-        rename and map column to bool if
-"""
 from collections import namedtuple
 
 NEXT_ACTIVITY = 'next_activity'
@@ -33,8 +12,11 @@ THRESHOLD_CUSTOM = 'threshold_custom'
 
 class LabelContainer(namedtuple('LabelContainer', ["type", "attribute_name", "threshold_type", "threshold",
                                                    "add_remaining_time", "add_elapsed_time"])):
+    """Inner object describing labelling state.
+    For no labelling use NO_LABEL
+    """
+
     def __new__(cls, type=REMAINING_TIME, attribute_name=None, threshold_type=THRESHOLD_MEAN, threshold=0,
                 add_remaining_time=False, add_elapsed_time=False):
         return super(LabelContainer, cls).__new__(cls, type, attribute_name, threshold_type, threshold,
                                                   add_remaining_time, add_elapsed_time)
-

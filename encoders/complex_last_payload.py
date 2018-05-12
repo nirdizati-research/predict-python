9aac990c18130fa0586274e30073d73f969b8f36
Tõnis Kasekamp <tonis.kasekamp@salemove.com>
2018-05-12 11:11:07 +0300

    Complex all in one

--- encoders/complex_last_payload.py
+++ encoders/complex_last_payload.py
@@ -1,57 +1,42 @@
 import pandas as pd
 from opyenxes.classification.XEventAttributeClassifier import XEventAttributeClassifier
+from opyenxes.model import XTrace
 
-from encoders.label_container import LabelContainer, ATTRIBUTE_STRING, ATTRIBUTE_NUMBER
-from encoders.simple_index import add_label_columns, add_labels
-from log_util.log_metrics import events_by_date, resources_by_date, new_trace_start
+from encoders.encoding_container import EncodingContainer
+from encoders.label_container import LabelContainer
+from encoders.simple_index import add_label_columns, add_labels, setup_attribute_classifier, get_intercase_attributes
 
 CLASSIFIER = XEventAttributeClassifier("Trace name", ["concept:name"])
 ATTRIBUTE_CLASSIFIER = None
 
 
-def complex(log, label: LabelContainer, additional_columns: list, prefix_length=1, zero_padding=False, ):
-    return encode_complex_latest(log, label, prefix_length, additional_columns, columns_complex, data_complex,
-                                 zero_padding, is_complex=True)
+def complex(log: list, label: LabelContainer, encoding: EncodingContainer, additional_columns: list):
+    return encode_complex_latest(log, label, encoding, additional_columns, columns_complex, data_complex)
 
 
-def last_payload(log, label: LabelContainer, additional_columns: list, prefix_length=1, zero_padding=False):
-    return encode_complex_latest(log, label, prefix_length, additional_columns, columns_last_payload, data_last_payload,
-                                 zero_padding)
+def last_payload(log, label: LabelContainer, encoding: EncodingContainer, additional_columns: list):
+    return encode_complex_latest(log, label, encoding, additional_columns, columns_last_payload, data_last_payload)
 
 
-def encode_complex_latest(log, label: LabelContainer, prefix_length: int, additional_columns: list, column_fun,
-                          data_fun, zero_padding: bool, is_complex=False):
-    columns = column_fun(prefix_length, additional_columns, label)
+def encode_complex_latest(log: list, label: LabelContainer, encoding: EncodingContainer, additional_columns: list,
+                          column_fun, data_fun):
+    columns = column_fun(encoding.prefix_length, additional_columns, label)
     encoded_data = []
 
-    # Create classifier only once
-    if label.type == ATTRIBUTE_STRING or label.type == ATTRIBUTE_NUMBER:
-        global ATTRIBUTE_CLASSIFIER
-        ATTRIBUTE_CLASSIFIER = XEventAttributeClassifier("Attr class", [label.attribute_name])
-    # Expensive operations
-    executed_events = events_by_date([log]) if label.add_executed_events else None
-    resources_used = resources_by_date([log]) if label.add_resources_used else None
-    new_traces = new_trace_start([log]) if label.add_new_traces else None
+    setup_attribute_classifier(label)
+    kwargs = get_intercase_attributes(log, label)
     for trace in log:
-        if zero_padding and is_complex:
-            zero_count = (prefix_length - len(trace)) * (1 + len(additional_columns))
-        elif zero_padding:
-            zero_count = prefix_length - len(trace)
-            if zero_count > 0:
-                zero_count + len(additional_columns)
-        elif len(trace) <= prefix_length - 1:
-            # no padding, skip this trace
+        if len(trace) <= encoding.prefix_length - 1 and not encoding.is_zero_padding():
+            # trace too short and no zero padding
             continue
-        trace_row = []
-        trace_name = CLASSIFIER.get_class_identity(trace)
-        trace_row.append(trace_name)
-        # prefix_length - 1 == index
-        trace_row += data_fun(trace, prefix_length, additional_columns)
-        if zero_padding:
-            trace_row += ['0' for _ in range(0, zero_count)]
-        trace_row += add_labels(label, prefix_length, trace, ATTRIBUTE_CLASSIFIER=ATTRIBUTE_CLASSIFIER,
-                                executed_events=executed_events, resources_used=resources_used, new_traces=new_traces)
-        encoded_data.append(trace_row)
+        if encoding.is_all_in_one():
+            for i in range(1, encoding.prefix_length + 1):
+                encoded_data.append(
+                    trace_to_row(trace, encoding, i, data_fun, additional_columns=additional_columns, **kwargs))
+        else:
+            encoded_data.append(
+                trace_to_row(trace, encoding, encoding.prefix_length, data_fun, additional_columns=additional_columns,
+                             **kwargs))
 
     return pd.DataFrame(columns=columns, data=encoded_data)
 
@@ -117,3 +102,41 @@ def data_last_payload(trace: list, prefix_length: int, additional_columns: list)
             value = event_attrs.get(att).get_value()
         data.append(value)
     return data
+
+
+def trace_to_row(trace: XTrace, encoding: EncodingContainer, event_index: int, data_fun, label=None,
+                 executed_events=None, resources_used=None, new_traces=None, additional_columns=None):
+    zero_count = get_zero_count(encoding, event_index, len(trace), len(additional_columns))
+    trace_row = []
+    trace_name = CLASSIFIER.get_class_identity(trace)
+    trace_row.append(trace_name)
+    # prefix_length - 1 == index
+    trace_row += data_fun(trace, event_index, additional_columns)
+    if encoding.is_zero_padding() or encoding.is_all_in_one():
+        trace_row += ['0' for _ in range(0, zero_count)]
+    trace_row += add_labels(label, event_index, trace, ATTRIBUTE_CLASSIFIER=ATTRIBUTE_CLASSIFIER,
+                            executed_events=executed_events, resources_used=resources_used, new_traces=new_traces)
+    return trace_row
+
+
+def get_zero_count(encoding: EncodingContainer, event_index: int, trace_len: int, add_columns_len: int):
+    # Don't know what to call these
+    a = encoding.prefix_length - event_index
+    b = encoding.prefix_length - trace_len
+    if encoding.is_all_in_one() and encoding.is_complex():
+        if a < b:
+            a = b
+        zero_count = a * (1 + add_columns_len)
+    elif encoding.is_all_in_one():
+        zero_count = a
+        if zero_count > 0:
+            zero_count + add_columns_len
+    elif encoding.is_zero_padding() and encoding.is_complex():
+        zero_count = b * (1 + add_columns_len)
+    elif encoding.is_zero_padding():
+        zero_count = b
+        if zero_count > 0:
+            zero_count + add_columns_len
+    else:
+        zero_count = 0
+    return zero_count

bb4edbfb3a566f952de5baaa9e70821b2b71b7d7
Tõnis Kasekamp <tonis.kasekamp@salemove.com>
2018-06-04 18:44:00 +0300

    Deployment scripts

--- deployment.sh
+++ deployment.sh
@@ -4,3 +4,5 @@ sudo pkill -f python
 sudo nohup python3 manage.py runserver 0.0.0.0:80 &
 sudo nohup python3 manage.py rqworker default &
 sudo nohup python3 manage.py rqworker default &
+sudo nohup python3 manage.py rqworker default &
+sudo nohup python3 manage.py rqworker default &

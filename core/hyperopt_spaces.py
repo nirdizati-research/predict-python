117598722185fbb320ce3f37392acbdb155ad7d4
Willo <wrizzi@fbk.eu>
2018-08-09 09:25:07 +0200

    added support to hyperopt for incremental naive bayes, hoeffding tree and adaptive hoeffding tree"

--- core/hyperopt_spaces.py
+++ core/hyperopt_spaces.py
@@ -11,10 +11,11 @@ def get_space(job: dict):
 
 
 def _classification_random_forest():
-    return {'n_estimators': hp.choice('n_estimators', np.arange(150, 1000, dtype=int)),
-            'max_depth': scope.int(hp.quniform('max_depth', 4, 30, 1)),
-            'max_features': hp.uniform('max_features', 0.0, 1.0)
-            }
+    return {
+        'n_estimators': hp.choice('n_estimators', np.arange(150, 1000, dtype=int)),
+        'max_depth': scope.int(hp.quniform('max_depth', 4, 30, 1)),
+        'max_features': hp.uniform('max_features', 0.0, 1.0)
+    }
 
 
 # test case dynamic max feature
@@ -37,6 +38,43 @@ def _classification_xgboost():
     return {
         'n_estimators': hp.choice('n_estimators', np.arange(150, 1000, dtype=int)),
         'max_depth': scope.int(hp.quniform('max_depth', 3, 30, 1)),
+    }
+
+
+def _classification_incremental_naive_bayes():
+    return {
+        'alpha': hp.uniform('alpha', 0.001, 10.0),
+        'fit_prior': hp.choice('fit_prior', [ True, False ])
+    }
+
+
+def _classification_incremental_adaptive_tree():
+    return {
+        'grace_period': hp.uniform('grace_period', 1, 200),
+        'split_criterion': hp.choice('split_criterion', [ 'gini', 'info_gain' ]),
+        'split_confidence': hp.uniform('split_confidence', .0000001, .0000009),
+        'tie_threshold': hp.uniform('tie_threshold', 0.01, .09),
+        'binary_split': hp.choice('binary_split', [ True, False ]),
+        'stop_mem_management': hp.choice('stop_mem_management', [ True, False ]),
+        'remove_poor_atts': hp.choice('remove_poor_atts', [ True, False ]),
+        'no_preprune': hp.choice('no_preprune', [ True, False ]),
+        'leaf_prediction': hp.choice('leaf_prediction', [ 'mc', 'nb', 'nba']),
+        'nb_threshold': hp.uniform('nb_threshold', 0.0, 1.0)
+    }
+
+
+def _classification_incremental_hoeffding_tree():
+    return {
+        'grace_period': hp.uniform('grace_period', 1, 200),
+        'split_criterion': hp.choice('split_criterion', [ 'gini', 'info_gain' ]),
+        'split_confidence': hp.uniform('split_confidence', .0000001, .0000009),
+        'tie_threshold': hp.uniform('tie_threshold', 0.01, .09),
+        'binary_split': hp.choice('binary_split', [ True, False ]),
+        'stop_mem_management': hp.choice('stop_mem_management', [ True, False ]),
+        'remove_poor_atts': hp.choice('remove_poor_atts', [ True, False ]),
+        'no_preprune': hp.choice('no_preprune', [ True, False ]),
+        'leaf_prediction': hp.choice('leaf_prediction', [ 'mc', 'nb', 'nba']),
+        'nb_threshold': hp.uniform('nb_threshold', 0.0, 1.0)
     }
 
 
@@ -70,8 +108,16 @@ def _regression_xgboost():
     }
 
 
-HYPEROPT_SPACE_MAP = {CLASSIFICATION_RANDOM_FOREST: _classification_random_forest,
-                      CLASSIFICATION_KNN: _classification_knn, CLASSIFICATION_XGBOOST: _classification_xgboost,
-                      CLASSIFICATION_DECISION_TREE: _classification_decision_tree,
-                      REGRESSION_RANDOM_FOREST: _regression_random_forest, REGRESSION_XGBOOST: _regression_xgboost,
-                      REGRESSION_LASSO: _regression_lasso, REGRESSION_LINEAR: _regression_linear}
+HYPEROPT_SPACE_MAP = {
+    CLASSIFICATION_RANDOM_FOREST: _classification_random_forest,
+    CLASSIFICATION_KNN: _classification_knn,
+    CLASSIFICATION_XGBOOST: _classification_xgboost,
+    CLASSIFICATION_DECISION_TREE: _classification_decision_tree,
+    CLASSIFICATION_INCREMENTAL_NAIVE_BAYES: _classification_incremental_naive_bayes,
+    CLASSIFICATION_INCREMENTAL_ADAPTIVE_TREE: _classification_incremental_adaptive_tree,
+    CLASSIFICATION_INCREMENTAL_HOEFFDING_TREE: _classification_incremental_hoeffding_tree,
+    REGRESSION_RANDOM_FOREST: _regression_random_forest,
+    REGRESSION_XGBOOST: _regression_xgboost,
+    REGRESSION_LASSO: _regression_lasso,
+    REGRESSION_LINEAR: _regression_linear
+}

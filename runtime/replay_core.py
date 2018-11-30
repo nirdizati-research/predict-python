924f608e43cba59d1b32eebea7409317e1eb9bbd
mrsonuk <mrsonuk@users.noreply.github.com>
2018-11-30 18:36:58 +0200

    Update replay_core.py

--- runtime/replay_core.py
+++ runtime/replay_core.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
 import json
+import csv
 import xml.etree.ElementTree as Et
 import datetime
+import subprocess
 
 from dateutil.parser import parse as dateparser
 from opyenxes.factory.XFactory import XFactory
@@ -14,7 +17,9 @@ from runtime.models import XTrace, XEvent, XLog, DemoReplayer
 from jobs.ws_publisher import publish
 
 
-def prepare(ev, tr, lg, replayer_id, reg_id, class_id, real_log, end=False):
+
+
+def prepare(ev, tr, lg, replayer_id, reg_id, class_id, real_log, nn=False, end=False):
     if int(reg_id) > 0:
         reg_model = PredModels.objects.get(pk=reg_id)
     else:
@@ -69,67 +74,70 @@ def prepare(ev, tr, lg, replayer_id, reg_id, class_id, real_log, end=False):
         event = XEvent.objects.create(config=event_map, trace=trace, xid=event_xid.__str__())
 
     events = XEvent.objects.filter(trace=trace, pk__lte=event.id)
-
-    run_log = run.create_log(XAttributeMap(json.loads(log.config)))
-    run_trace = run.create_trace(XAttributeMap(json.loads(trace.config)))
-    c = 0
-
-    for event in events:
-        c = c + 1
-        evt = run.create_event(XAttributeMap(json.loads(event.config)))
-        run_trace.append(evt)
-    run_log.append(run_trace)
-    if c == 1:
-        trace.first_event = str(xmap.get('time:timestamp'))
-    trace.last_event = str(xmap.get('time:timestamp'))
     
-    trace.duration = datetime.timedelta.total_seconds(dateparser(str(trace.last_event)) - dateparser(str(trace.first_event)))
-    trace.n_events = c
-    trace.save()
-    error = True
+    if nn:
+        next_activities(events, trace)
+    else:    
+        run_log = run.create_log(XAttributeMap(json.loads(log.config)))
+        run_trace = run.create_trace(XAttributeMap(json.loads(trace.config)))
+        c = 0
     
-    try:
-        if trace.reg_model is not None:
-            if trace.reg_model.config['encoding']['padding'] != ZERO_PADDING and trace.reg_model.config['encoding'][
-                'generation_type'] != ALL_IN_ONE:
-                reg_config = trace.reg_model.config
-                reg_config['encoding']['prefix_length'] = c
-                right_reg_model = PredModels.objects.filter(config=reg_config)
-                trace.reg_model = right_reg_model[0]
-            result_data = runtime_calculate(run_log, trace.reg_model.to_dict())
-            trace.reg_results = result_data['prediction']
-            trace.reg_actual = result_data['label']
+        for event in events:
+            c = c + 1
+            evt = run.create_event(XAttributeMap(json.loads(event.config)))
+            run_trace.append(evt)
+        run_log.append(run_trace)
+        if c == 1:
+            trace.first_event = str(xmap.get('time:timestamp'))
+        trace.last_event = str(xmap.get('time:timestamp'))
+        
+        trace.duration = datetime.timedelta.total_seconds(dateparser(str(trace.last_event)) - dateparser(str(trace.first_event)))
+        trace.n_events = c
+        trace.save()
+        error = True
+        
+        try:
+            if trace.reg_model is not None:
+                if trace.reg_model.config['encoding']['padding'] != ZERO_PADDING and trace.reg_model.config['encoding'][
+                    'generation_type'] != ALL_IN_ONE:
+                    reg_config = trace.reg_model.config
+                    reg_config['encoding']['prefix_length'] = c
+                    right_reg_model = PredModels.objects.filter(config=reg_config)
+                    trace.reg_model = right_reg_model[0]
+                result_data = runtime_calculate(run_log, trace.reg_model.to_dict())
+                trace.reg_results = result_data['prediction']
+                trace.reg_actual = result_data['label']
+                trace.save()
+        except Exception as e:
+            error = False
+            print("An exception has occurred in regression, error:" + str(e.__repr__()))
+            trace.error = str(e.__repr__())
             trace.save()
-    except Exception as e:
-        error = False
-        print("An exception has occurred in regression, error:" + str(e.__repr__()))
-        trace.error = str(e.__repr__())
-        trace.save()
-        raise e
-    try:
-        if trace.class_model is not None:
-            if trace.class_model.config['encoding']['padding'] != ZERO_PADDING and trace.class_model.config['encoding'][
-                'generation_type'] != ALL_IN_ONE:
-                class_config = trace.class_model.config
-                class_config['encoding']['prefix_length'] = c
-                right_class_model = PredModels.objects.filter(config=class_config)
-                trace.class_model = right_class_model[0]
-            result_data = runtime_calculate(run_log, trace.class_model.to_dict())
-            trace.class_results = result_data['prediction']
-            trace.class_actual = result_data['label']
+            raise e
+        try:
+            if trace.class_model is not None:
+                if trace.class_model.config['encoding']['padding'] != ZERO_PADDING and trace.class_model.config['encoding'][
+                    'generation_type'] != ALL_IN_ONE:
+                    class_config = trace.class_model.config
+                    class_config['encoding']['prefix_length'] = c
+                    right_class_model = PredModels.objects.filter(config=class_config)
+                    trace.class_model = right_class_model[0]
+                result_data = runtime_calculate(run_log, trace.class_model.to_dict())
+                trace.class_results = result_data['prediction']
+                trace.class_actual = result_data['label']
+                trace.save()
+        except Exception as e:
+            trace.error = str(e.__repr__())
+            error = False
             trace.save()
-    except Exception as e:
-        trace.error = str(e.__repr__())
-        error = False
-        trace.save()
-        print("An exception has occurred in classification, error:" + str(e.__repr__()))
-        raise e
-    finally:
-        if error:
-            trace.error = ""
-        trace.save()
-        publish(trace)
-
+            print("An exception has occurred in classification, error:" + str(e.__repr__()))
+            raise e
+        finally:
+            if error:
+                trace.error = ""
+            trace.save()
+            publish(trace)
+        
 
 def parse(xml):
     element = Et.fromstring(xml.encode("utf-8"))
@@ -141,3 +149,30 @@ def xMap_to_dict(xmap):
     for key in xmap.keys():
         d[key] = str(xmap.get(key))
     return d
+
+def next_activities(events, trace):
+    event_log = open('../Process-Sequence-Prediction-with-A-priori-knowledge/data/Sepsis_test.csv', 'w')
+    csvwriter = csv.writer(event_log, delimiter=';')
+    log_headers = ['CaseID', 'ActivityID', 'CompleteTimestamp']
+    csvwriter.writerow(log_headers)
+    for event in events:
+        event_row = []
+        ev_xmap = XAttributeMap(json.loads(event.config))
+        tr_xmap = XAttributeMap(json.loads(trace.config))
+        event_row.append(tr_xmap.get('concept:name'))
+        event_row.append(ev_xmap.get('concept:name'))
+        tstamp = ev_xmap.get('time:timestamp')
+        event_row.append(tstamp[:-6])
+        csvwriter.writerow(event_row)
+    event_log.close()
+    py2command = "/home/stefano/Scrivania/Tirocinio/right/Process-Sequence-Prediction-with-A-priori-knowledge/src/support_scripts/csv_converter.py"
+    process = subprocess.Popen(py2command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    exitcode = process.wait()
+    py2commandrun = "/home/stefano/Scrivania/Tirocinio/right/Process-Sequence-Prediction-with-A-priori-knowledge/src/evaluate_next_activity_and_time.py"
+    process = subprocess.Popen(py2commandrun.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    exitcode = process.wait()
+    
+        
+    

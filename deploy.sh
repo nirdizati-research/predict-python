83bb5c0a0513b5567f696e9fb3d6a8c7b1cde399
Williams Rizzi <rizzi.williams@me.com>
2018-12-17 17:54:24 +0100

    wrong usage of scale option , fixed

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ export DOCKER_HOST=unix:///tmp/docker.sock
 docker-compose pull
 
 # Start up the new containers
-docker-compose up --detach --force-recreate redis server scheduler --scale worker=4
+docker-compose up --detach --force-recreate redis server scheduler
 docker-compose up --detach --force-recreate --scale worker=4
 
 # Close the SSH connection using the control socket opened previously

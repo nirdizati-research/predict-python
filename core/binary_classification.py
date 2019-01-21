115f2456c387c9dfc630a296c97177f1b6f96db0
Williams Rizzi <rizzi.williams@me.com>
2019-01-21 10:10:15 +0100

    added balancing function for better automatic threshold calculation

--- core/binary_classification.py
+++ core/binary_classification.py
@@ -104,6 +104,42 @@ def kmeans_clustering_test(test_data, clf, estimator, testing=False):
     return result_data, auc
 
 
+#TODO: SUBSAMPLE manually balance data
+def balanced_subsample(x, y, subsample_size=1.0):
+
+    class_xs = []
+    min_elems = None
+
+    for yi in np.unique(y):
+        elems = x[(y == yi)]
+        class_xs.append((yi, elems))
+        if min_elems == None or elems.shape[0] < min_elems:
+            min_elems = elems.shape[0]
+
+    use_elems = min_elems
+    if subsample_size < 1:
+        use_elems = int(min_elems * subsample_size)
+
+    xs = []
+    ys = []
+
+    for ci, this_xs in class_xs:
+        if len(this_xs) > use_elems:
+            np.random.shuffle(this_xs)
+
+        x_ = this_xs[:use_elems]
+        y_ = np.empty(use_elems)
+        y_.fill(ci)
+
+        xs.append(x_)
+        ys.append(y_)
+
+    xs = np.concatenate(xs)
+    ys = np.concatenate(ys)
+
+    return xs, ys
+
+
 def no_clustering_train(original_test_data, train_data, clf):
     y = train_data['actual']
     # if isinstance(clf, MultinomialNB):

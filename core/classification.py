db7305178fbc73ad2ff73378bf1a396b6104167d
Williams Rizzi <rizzi.williams@me.com>
2019-02-08 07:49:50 +0100

    added the fallback to the clone method, removed old code, ada and hoef are still showing some issues with indexing of Dataframes

--- core/classification.py
+++ core/classification.py
@@ -1,10 +1,8 @@
-from typing import Any
 
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
-from sklearn import metrics, clone
-from sklearn.cluster import KMeans
+from sklearn import clone
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.externals import joblib
 from sklearn.linear_model import Perceptron, SGDClassifier
@@ -16,11 +14,10 @@ from xgboost import XGBClassifier
 
 from core.clustering import Clustering
 from core.common import get_method_config
-from core.constants import KMEANS, NO_CLUSTER
 from core.constants import KNN, RANDOM_FOREST, DECISION_TREE, XGBOOST, MULTINOMIAL_NAIVE_BAYES, ADAPTIVE_TREE, \
     HOEFFDING_TREE, SGDCLASSIFIER, PERCEPTRON
 from encoders.label_container import REMAINING_TIME, ATTRIBUTE_NUMBER, DURATION, NEXT_ACTIVITY, ATTRIBUTE_STRING
-from utils.result_metrics import calculate_auc, calculate_results_classification, _get_auc
+from utils.result_metrics import calculate_results_classification, _get_auc
 
 pd.options.mode.chained_assignment = None
 
@@ -68,16 +65,20 @@ def _train(job: dict, train_data: DataFrame, classifier) -> dict:
 
         x = train_data[cluster]
         if not x.empty:
-            y = x['label']
+            y = DataFrame(x['label'])
             try:
                 classifier.fit(x.drop('label', 1), y)
             except NotImplementedError:
-                classifier.partial_fit(x.drop('label', 1).values, y)
+                classifier.partial_fit(x.drop('label', 1), y)
             except Exception as e:
                 raise e
 
             models[cluster] = classifier
-            classifier = clone(classifier)
+            try:
+                classifier = clone(classifier)
+            except TypeError:
+                classifier = clone(classifier, safe=False)
+                classifier.reset()
 
     return {'clusterer': clusterer, 'classifier': models}
 
@@ -119,158 +120,6 @@ def _test(model_split: dict, data: DataFrame, evaluation: bool, is_binary_classi
         pass  # TODO: check if AUC is ok for multiclass, otherwise implement
 
     return results_df, auc
-
-
-# def classification(training_df: DataFrame, test_df: DataFrame, job: dict):
-#     is_binary_classifier = _check_is_binary_classifier(job['label'].type)
-#
-#     classifier = _choose_classifier(job)
-#
-#     train_data, test_data, original_test_data = _drop_columns(training_df, test_df)
-#
-#     if job['clustering'] == KMEANS:
-#         results_df, auc, model_split = _kmeans_clustering_train(original_test_data, train_data, classifier,
-#                                                                 job[KMEANS], is_binary_classifier)
-#     elif job['clustering'] == NO_CLUSTER:
-#         results_df, auc, model_split = _no_clustering_train(original_test_data, train_data, classifier,
-#                                                             is_binary_classifier)
-#     else:
-#         raise ValueError("Unexpected clustering {}".format(job['clustering']))
-#
-#     results = _prepare_results(results_df, auc)
-#     return results, model_split
-#
-#
-# def classification_single_log(run_df: DataFrame, model: dict):
-#     result = None
-#     is_binary_classifier = _check_is_binary_classifier(model['label'].type)
-#
-#     split = model['split']
-#     results = dict()
-#     results['label'] = run_df['label']
-#     if is_binary_classifier:
-#         run_df = run_df.drop('label', 1)
-#
-#     if split['type'] == NO_CLUSTER:
-#         clf = joblib.load(split['model_path'])
-#         result, _ = _no_clustering_test(run_df, clf)
-#     elif split['type'] == KMEANS:
-#         clf = joblib.load(split['model_path'])
-#         estimator = joblib.load(split['estimator_path'])
-#         result, _ = _kmeans_clustering_test(run_df, clf, estimator, is_binary_classifier)
-#     results['prediction'] = result['predicted']
-#     return results
-#
-#
-# def _kmeans_clustering_train(original_test_data, train_data, classifier, kmeans_config: dict,
-#                              is_binary_classifier: bool):
-#     estimator = KMeans(**kmeans_config)
-#     models = dict()
-#
-#     estimator.fit(train_data.drop('label', 1))
-#     cluster_lists = {i: train_data.iloc[np.where(estimator.labels_ == i)[0]] for i in range(estimator.n_clusters)}
-#     for i, cluster_list in cluster_lists.items():
-#         clustered_train_data = cluster_list
-#         if clustered_train_data.shape[0] == 0:
-#             pass
-#         else:
-#             y = clustered_train_data['label']
-#             try:
-#                 classifier.fit(clustered_train_data.drop('label', 1), y)
-#             except:
-#                 classifier.partial_fit(clustered_train_data.drop('label', 1).values, y)
-#             models[i] = classifier
-#     model_split = dict()
-#     model_split['type'] = KMEANS
-#     model_split['estimator'] = estimator
-#     model_split['model'] = models
-#     result, auc = _kmeans_clustering_test(original_test_data, models, estimator, is_binary_classifier, testing=True)
-#     return result, auc, model_split
-#
-#
-# def _kmeans_clustering_test(test_data, classifier, estimator, is_binary_classifier: bool, testing: bool = False):
-#     drop_list = ['trace_id', 'label'] if testing else ['trace_id']
-#     auc = 0
-#     counter = 0
-#
-#     test_cluster_lists = {
-#         i: test_data.iloc[np.where(estimator.predict(test_data.drop(drop_list, 1)) == i)[0]]
-#         for i in range(estimator.n_clusters)}
-#
-#     result_data = None
-#     for i, cluster_list in test_cluster_lists.items():
-#         counter += 1
-#         original_clustered_test_data = cluster_list
-#         if original_clustered_test_data.shape[0] == 0:
-#             pass
-#         else:
-#             clustered_test_data = original_clustered_test_data.drop(drop_list, 1)
-#
-#             try:
-#                 prediction = classifier[i].predict(clustered_test_data)
-#             except:
-#                 prediction = classifier[i].predict(clustered_test_data.values)
-#             original_clustered_test_data["predicted"] = prediction
-#
-#             if is_binary_classifier:
-#                 if hasattr(classifier[i], 'predict_proba'):
-#                     try:
-#                         scores = classifier[i].predict_proba(clustered_test_data)
-#                     except:
-#                         scores = classifier[i].predict_proba(clustered_test_data.values)
-#                     if testing:
-#                         actual = original_clustered_test_data['label']
-#                         auc = calculate_auc(actual, scores, auc)
-#             else:
-#                 if testing:
-#                     original_clustered_test_data["actual"] = original_clustered_test_data['label']
-#
-#             if result_data is None:
-#                 result_data = original_clustered_test_data
-#             else:
-#                 result_data = result_data.append(original_clustered_test_data)
-#     if testing:
-#         try:
-#             auc = float(auc) / counter
-#         except ZeroDivisionError:
-#             auc = 0
-#     return result_data, auc
-#
-#
-# def _no_clustering_train(original_test_data, train_data, classifier: Any, is_binary_classifier: bool):
-#     y = train_data['label']
-#     try:
-#         classifier.fit(train_data.drop('label', 1), y)
-#     except NotImplementedError:
-#         classifier.partial_fit(train_data.drop('label', 1).values, y)
-#     except Exception as e:
-#         raise e
-#
-#     actual = original_test_data['label']
-#     original_test_data, scores = _no_clustering_test(original_test_data, classifier, True)
-#
-#     auc = 0
-#     if is_binary_classifier or len(set(actual)) <= 2:
-#         auc = _get_auc(actual, scores)
-#     else:
-#         pass  # TODO: check if AUC is ok for multiclass, otherwise implement
-#     model_split = dict()
-#     model_split['type'] = NO_CLUSTER
-#     model_split['model'] = classifier
-#     return original_test_data, auc, model_split
-#
-#
-# def _no_clustering_test(test_data, classifier, testing=False):
-#     scores = 0
-#     if testing:
-#         if hasattr(classifier, 'decision_function'):
-#             scores = classifier.decision_function(test_data.drop(['trace_id', 'label'], 1))
-#         else:
-#             scores = classifier.predict_proba(test_data.drop(['trace_id', 'label'], 1))
-#             if np.size(scores, 1) >= 2:  # checks number of columns
-#                 scores = scores[:, 1]
-#     test_data['predicted'] = classifier.predict(test_data.drop(['trace_id', 'label'], 1))
-#     return test_data, scores
 
 
 def _prepare_results(df: DataFrame, auc: int):
--- core/regression.py
+++ core/regression.py
@@ -1,8 +1,6 @@
-import numpy as np
 import pandas as pd
 from pandas import DataFrame
 from sklearn import clone
-from sklearn.cluster import KMeans
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.externals import joblib
 from sklearn.linear_model import Lasso
@@ -11,7 +9,7 @@ from xgboost import XGBRegressor
 
 from core.clustering import Clustering
 from core.common import get_method_config
-from core.constants import KMEANS, LINEAR, RANDOM_FOREST, LASSO, NO_CLUSTER, XGBOOST
+from core.constants import LINEAR, RANDOM_FOREST, LASSO, XGBOOST
 from utils.result_metrics import calculate_results_regression
 
 pd.options.mode.chained_assignment = None
@@ -59,7 +57,10 @@ def _train(job: dict, train_data: DataFrame, regressor) -> dict:
             regressor.fit(x.drop('label', 1), y)
 
             models[cluster] = regressor
-            regressor = clone(regressor)
+            try:
+                regressor = clone(regressor)
+            except TypeError:
+                regressor = clone(regressor, safe=False)
 
     return {'clusterer': clusterer, 'regressor': models}
 
@@ -78,99 +79,6 @@ def _test(model_split, data: DataFrame) -> dict:
             x['predicted'] = regressor[cluster].predict(x.drop('label', 1))
             results_df = results_df.append(x)
     return results_df
-
-
-# def regression(training_df: DataFrame, test_df: DataFrame, job: dict):
-#     regressor = _choose_regressor(job)
-#
-#     train_data, test_data, original_test_data = _prep_data(training_df, test_df)
-#
-#     if job['clustering'] == KMEANS:
-#         results_df, model_split = _kmeans_clustering_train(original_test_data, train_data, regressor, job['kmeans'])
-#     else:
-#         results_df, model_split = _no_clustering_train(original_test_data, train_data, test_data, regressor)
-#
-#     results = calculate_results_regression(results_df, job['label'])
-#     return results, model_split
-#
-#
-# def regression_single_log(run_df: DataFrame, model):
-#     split = model['split']
-#     results = dict()
-#     results['label'] = run_df['label']
-#     run_df = run_df.drop(['label'], 1)
-#     if 'elapsed_time' in run_df.columns:
-#         run_df = run_df.drop(['elapsed_time'], 1)
-#     run_df = run_df.drop('trace_id', 1)
-#
-#     if split['type'] == NO_CLUSTER:
-#         regressor = joblib.load(split['model_path'])
-#         results_data = _no_clustering_test(run_df, run_df, regressor)
-#     elif split['type'] == KMEANS:
-#         regressor = joblib.load(split['model_path'])
-#         estimator = joblib.load(split['estimator_path'])
-#         results_data = _kmeans_clustering_test(run_df, regressor, estimator)
-#     else:
-#         raise ValueError('Unexpected split type {}'.format(split['type']))
-#     results['prediction'] = results_data['prediction']
-#     return results
-#
-#
-# def _kmeans_clustering_train(original_test_data, train_data, regressor, kmeans_dict: dict):
-#     estimator = KMeans(**kmeans_dict)
-#     estimator.fit(train_data)
-#     cluster_lists = {i: train_data.iloc[np.where(estimator.labels_ == i)[0]] for i in range(estimator.n_clusters)}
-#     models = dict()
-#     for i, cluster_list in cluster_lists.items():
-#         clustered_train_data = cluster_list
-#         if clustered_train_data.shape[0] == 0:
-#             pass
-#         else:
-#             y = clustered_train_data['label']
-#             clustered_train_data = clustered_train_data.drop('label', 1)
-#
-#             regressor.fit(clustered_train_data, y)
-#             models[i] = regressor
-#     model_split = dict()
-#     model_split['type'] = KMEANS
-#     model_split['estimator'] = estimator
-#     model_split['model'] = models
-#     return _kmeans_clustering_test(original_test_data, models, estimator, testing=True), model_split
-#
-#
-# def _kmeans_clustering_test(test_data, regressor, estimator, testing=False):
-#     drop_list = ['trace_id', 'label'] if testing else ['trace_id']
-#     test_cluster_lists = {
-#         i: test_data.iloc[np.where(estimator.predict(test_data.drop('trace_id', 1)) == i)[0]]
-#         for i in range(estimator.n_clusters)}
-#     result_data = None
-#     for i, cluster_list in test_cluster_lists.items():
-#         original_clustered_test_data = cluster_list
-#         if original_clustered_test_data.shape[0] == 0:
-#             pass
-#         else:
-#             clustered_test_data = original_clustered_test_data.drop(drop_list, 1)
-#             original_clustered_test_data['prediction'] = regressor[i].predict(clustered_test_data)
-#         if result_data is None:
-#             result_data = original_clustered_test_data
-#         else:
-#             result_data = result_data.append(original_clustered_test_data)
-#     return result_data
-#
-#
-# def _no_clustering_train(original_test_data, train_data, test_data, regressor):
-#     y = train_data['label']
-#     train_data = train_data.drop('label', 1)
-#     regressor.fit(train_data, y)
-#     model_split = dict()
-#     model_split['type'] = NO_CLUSTER
-#     model_split['model'] = regressor
-#     return _no_clustering_test(original_test_data, test_data, regressor), model_split
-#
-#
-# def _no_clustering_test(original_test_data, test_data, regressor):
-#     original_test_data['prediction'] = regressor.predict(test_data)
-#     return original_test_data
 
 
 def _prep_data(training_df: DataFrame, test_df: DataFrame):

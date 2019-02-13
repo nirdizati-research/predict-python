a7dcba19f80c961822279145f8be40f1b70406ac
HitLuca <luca.simonetto.94@gmail.com>
2019-02-13 10:15:12 +0100

    :truck: renamed nirdizati folder to nirdizati-research, with relative refactoring

--- nirdizati-research/wsgi.py
+++ nirdizati-research/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for nirdizati project.
+WSGI config for nirdizati-research project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nirdizati.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nirdizati-research.settings")
 
 application = get_wsgi_application()

0f1c2bb1a995127ac882e9c53888ffb4b9d09714
HitLuca <luca.simonetto.94@gmail.com>
2019-02-13 15:39:36 +0100

    :construction_worker: added repo condition check in order to skip deploy on forks

--- rebuild_migrations.sh
+++ rebuild_migrations.sh
@@ -5,4 +5,3 @@ find . -path "*/migrations/*.pyc"  -delete
 rm ./db/db.sqlite3
 
 REDIS_HOST=localhost REDIS_PORT=6379 pipenv run python manage.py makemigrations
-REDIS_HOST=localhost REDIS_PORT=6379 pipenv run python manage.py migrate

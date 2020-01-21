1d335c771a6553c83f63cee4f8790c5165018a2a
Musacca <38190552+Musacca@users.noreply.github.com>
2020-01-21 23:17:16 +0200

    prediction api

--- src/runtime/urls.py
+++ src/runtime/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^prediction/', views.post_prediction),
     url(r'^replay/', views.post_replay),
     url(r'^replay_prediction/', views.post_replay_prediction),
+    url(r'^predictions/(?P<pk>[0-9]+)&(?P<explanation_target>[0-9]+)', views.get_prediction),
+
 ]

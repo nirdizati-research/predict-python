97f2cc62d5891057cadd24b7905662be625dff9b
Musacca <mmmusabir@gmail.com>
2020-03-28 13:22:49 +0200

    get train and test log from split APIs ready

--- src/split/urls.py
+++ src/split/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 
-from src.logs.views import SplitDetail, upload_multiple
+from src.logs.views import SplitDetail, upload_multiple, get_split_train_logs, get_split_test_logs
 from src.split.views import SplitList
 
 urlpatterns = [
     url(r'^$', SplitList.as_view()),
     url(r'^(?P<pk>[0-9]+)$', SplitDetail.as_view()),
     url(r'multiple$', upload_multiple),
+    url(r'^(?P<pk>[0-9]+)/logs/train', get_split_train_logs),
+    url(r'^(?P<pk>[0-9]+)/logs/test', get_split_test_logs)
+
 ]
